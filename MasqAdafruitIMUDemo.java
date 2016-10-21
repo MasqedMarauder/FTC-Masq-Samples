1afae2288afd53f144c1ba76f0e29da4d82de4d1
MasqedMarauder <MasqedMarauder@users.noreply.github.com>
2016-10-20 21:16:51 -0400

    Create MasqAdafruitIMUDemo.java

---
+++ MasqAdafruitIMUDemo.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * This is a demo teleop program that shows how to use the MasqAdafruitIMU.
+ * Created by Varun Singh, Lead Programmer of Team 4997 Masquerade.
+ */
+
+@TeleOp(name = "MasqAdafruitIMUDemo", group = "Demo")
+public class MasqAdafruitIMUDemo extends LinearOpMode {
+
+    public void runOpMode() throws InterruptedException {
+        // in this demo, the IMU is named "IMU" in the robot configuration file
+        MasqAdafruitIMU imu = new MasqAdafruitIMU("IMU", hardwareMap);
+
+        // wait to see this on the Driver Station before pressing play, to make sure the IMU has been initialized
+        telemetry.addData("Status", "Initialization Complete");
+        telemetry.update();
+        
+        waitForStart();
+
+        while (opModeIsActive()) {
+            // the next 4 lines show how to retrieve angles from the imu and use them
+            double[] angles = imu.getAngles();
+            double yaw = angles[0];
+            double pitch = angles[1];
+            double roll = angles[2];
+            
+            // this adds telemetry data using the telemetrize() method in the MasqAdafruitIMU class
+            telemetry.addData(imu.getName(), imu.telemetrize());
+            telemetry.update();
+        }
+    }
+    
+}
